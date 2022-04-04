f9597192b3b07221e5ddddd6b7d2c4e472dec39f
mrdbrg <rdg.brg@gmail.com>
2022-04-03 22:38:13 -0400

    test without error with StepVerifier

--- learn-reactivespring/src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTest.java
+++ learn-reactivespring/src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTest.java
@@ -3,8 +3,29 @@ package com.learnreactivespring.fluxandmonoplayground;
 import org.junit.jupiter.api.Test;
 
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 public class FluxAndMonoTest {
+	
+	/*
+	 *  
+	 * public final Disposable subscribe(@Nullable
+     *                         Consumer<? super T> consumer,
+     *                         @Nullable
+     *                         Consumer<? super Throwable> errorConsumer,
+     *                         @Nullable
+     *                         Runnable completeConsumer)
+	 * 
+	 * Parameters:
+     *  consumer - the consumer to invoke on each value
+     *  errorConsumer - the consumer to invoke on error signal
+     *  completeConsumer - the consumer to invoke on complete signal
+     * Returns:
+     *  a new Disposable that can be used to cancel the underlying Subscription
+	 * 
+	 * https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Flux.html#subscribe--
+	 * 
+	 */
 	
 	@Test
 	public void fluxTest() {
@@ -18,25 +39,17 @@ public class FluxAndMonoTest {
 				(e) -> System.err.println("The exception is: " + e),
 				() -> System.out.println("==> Completed (v)"));
 		
-		/*
-		 *  
-		 * public final Disposable subscribe(@Nullable
-         *                         Consumer<? super T> consumer,
-         *                         @Nullable
-         *                         Consumer<? super Throwable> errorConsumer,
-         *                         @Nullable
-         *                         Runnable completeConsumer)
-		 * 
-		 * Parameters:
-         *  consumer - the consumer to invoke on each value
-         *  errorConsumer - the consumer to invoke on error signal
-         *  completeConsumer - the consumer to invoke on complete signal
-         * Returns:
-         *  a new Disposable that can be used to cancel the underlying Subscription
-		 * 
-		 * https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Flux.html#subscribe--
-		 * 
-		 */
+	}
+	
+	@Test
+	public void fluxTestElements_WithoutError() {
+		Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
+				.log();
 		
+		StepVerifier.create(stringFlux)
+			.expectNext("Spring")
+			.expectNext("Spring Boot")
+			.expectNext("Reactive Spring")
+			.verifyComplete();
 	}
 }
