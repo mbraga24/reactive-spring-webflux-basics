1a6ad8810697883936aa01575d5bf5534e86e5f7
mrdbrg <rdg.brg@gmail.com>
2022-04-03 22:19:57 -0400

    add consumer, errorConsumer and completeConsumer to subscribe() / add subscribe anatomy and link

--- learn-reactivespring/src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTest.java
+++ learn-reactivespring/src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTest.java
@@ -10,10 +10,33 @@ public class FluxAndMonoTest {
 	public void fluxTest() {
 		
 		Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
-				.concatWith(Flux.error(new RuntimeException("An Exception Occurred")));
+//				.concatWith(Flux.error(new RuntimeException("An Exception Occurred")))
+				.concatWith(Flux.just("After Error"))
+				.log(); // will log all events happening behind the scenes
 		
 		stringFlux.subscribe(System.out::println, 
-				(e) -> System.err.println(e));
+				(e) -> System.err.println("The exception is: " + e),
+				() -> System.out.println("==> Completed (v)"));
+		
+		/*
+		 *  
+		 * public final Disposable subscribe(@Nullable
+         *                         Consumer<? super T> consumer,
+         *                         @Nullable
+         *                         Consumer<? super Throwable> errorConsumer,
+         *                         @Nullable
+         *                         Runnable completeConsumer)
+		 * 
+		 * Parameters:
+         *  consumer - the consumer to invoke on each value
+         *  errorConsumer - the consumer to invoke on error signal
+         *  completeConsumer - the consumer to invoke on complete signal
+         * Returns:
+         *  a new Disposable that can be used to cancel the underlying Subscription
+		 * 
+		 * https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Flux.html#subscribe--
+		 * 
+		 */
 		
 	}
 }
