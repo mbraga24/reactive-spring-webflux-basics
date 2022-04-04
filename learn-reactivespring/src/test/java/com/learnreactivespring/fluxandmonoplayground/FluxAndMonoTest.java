233c15b110a0974de8ec2010b5f190e003d27e7f
mrdbrg <rdg.brg@gmail.com>
2022-04-03 21:45:49 -0400

    Exception

--- learn-reactivespring/src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTest.java
+++ learn-reactivespring/src/test/java/com/learnreactivespring/fluxandmonoplayground/FluxAndMonoTest.java
@@ -9,8 +9,11 @@ public class FluxAndMonoTest {
 	@Test
 	public void fluxTest() {
 		
-		Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring");
-		stringFlux.subscribe(System.out::println);
+		Flux<String> stringFlux = Flux.just("Spring", "Spring Boot", "Reactive Spring")
+				.concatWith(Flux.error(new RuntimeException("An Exception Occurred")));
+		
+		stringFlux.subscribe(System.out::println, 
+				(e) -> System.err.println(e));
 		
 	}
 }
