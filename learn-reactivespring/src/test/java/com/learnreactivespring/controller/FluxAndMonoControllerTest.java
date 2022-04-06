c94c6480cc3ff9ccc86af6f7a645525f8f4b9e0c
mrdbrg <rdg.brg@gmail.com>
2022-04-05 23:33:12 -0400

    FluxAndMonoControllerTest annotations

---
+++ learn-reactivespring/src/test/java/com/learnreactivespring/controller/FluxAndMonoControllerTest.java
@@ -0,0 +1,11 @@
+package com.learnreactivespring.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@WebFluxTest // will scan all the classes annotated as @Controller and @RestController
+public class FluxAndMonoControllerTest {
+	
+}
