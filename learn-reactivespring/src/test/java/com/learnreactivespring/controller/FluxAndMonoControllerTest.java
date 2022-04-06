4012be82de2e8e01cd39e5e63715229085c5b5b5
mrdbrg <rdg.brg@gmail.com>
2022-04-06 10:11:35 -0400

    controller test flux_approach1

--- learn-reactivespring/src/test/java/com/learnreactivespring/controller/FluxAndMonoControllerTest.java
+++ learn-reactivespring/src/test/java/com/learnreactivespring/controller/FluxAndMonoControllerTest.java
@@ -1,11 +1,42 @@
 package com.learnreactivespring.controller;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 @RunWith(SpringRunner.class)
-@WebFluxTest // will scan all the classes annotated as @Controller and @RestController
+@WebFluxTest // will scan for all the classes annotated as @Controller and @RestController
 public class FluxAndMonoControllerTest {
 	
+	//	WebFluxTest annotation is resposible for creating the 
+	//	instance of WebTestClient.
+	@Autowired
+	WebTestClient webTestClient; // webTestClient will act as a Subscriber
+	
+	@Test
+	public void flux_approach1() {
+		Flux<Integer> integerFlux = webTestClient.get().uri("/flux")
+				.accept(MediaType.APPLICATION_JSON_UTF8) // accept the media type. 
+				.exchange()                              // call that will invoke the endpoint
+				.expectStatus().isOk()                   // expecting the status to be Ok.
+				.returnResult(Integer.class)             // expecting the return result to be Integer
+				.getResponseBody();                      // get ResponseBody where the actual flux will be
+		
+		StepVerifier.create(integerFlux)                  // verify the values from Flux
+			.expectSubscription()                         // expect the Subscription to be sent to us from the given endpoint
+			.expectNext(1)                                // next steps we will check for each value
+			.expectNext(2)
+			.expectNext(3)
+			.expectNext(4)
+			.verifyComplete();						       // verify complete event
+		
+				
+	}
 }
